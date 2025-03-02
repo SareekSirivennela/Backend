1acff400058bff7bbc75e696df86cf6065c782b9
SareekSirivennela <sareeksirivennela@gmail.com>
2025-03-02 15:03:06 +0530

    Backend sucess

---
+++ models/Recipes.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose')
+
+const CreateRecipe=mongoose.Schema({
+    Name:{type:String,require:true},
+    Description:{type:String,require:true},
+    Ingredients:{type:String,require:true},
+    Instructions:{type:String,require:true},
+    ImageURL:{type:String,require:true},
+    CookingTime:{type:Number,require:true},
+})
